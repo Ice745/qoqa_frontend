fdf28172cc8a2966d94646605b746032ab7cfaa3
Ice745 <32453300+Ice745@users.noreply.github.com>
2018-01-09 15:34:51 +0000

    Update app.routing.ts

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,7 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component'
+import { NewUserFormComponent } from './pages/new-user-form/new-user-form.component'
 
 const appRoutes: Routes = [
     {
@@ -10,7 +11,10 @@ const appRoutes: Routes = [
     {
         path: 'login',
         component: LoginPageComponent
-    }
+    },
+  {
+    path: 'signup',
+    component: NewUserFormComponent
 ];
 
 export const routing: ModuleWithProviders=RouterModule.forRoot(appRoutes);
